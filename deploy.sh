cedb7aa829a446682e165cb14ff0fe4c4c198a11
Christian Dräger <christian.draeger@idealo.de>
2017-06-10 23:09:23 +0200

    remove unused files

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Make Sure we are in the Build Directory"
-cd $TRAVIS_BUILD_DIR
-
-echo "Tag release"
-git tag ${VERSION} -am "${VERSION}"
-
-echo "Push commit and tags back to the repo"
-sudo git push origin master && git push origin master --tags
