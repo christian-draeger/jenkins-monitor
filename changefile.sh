1cf73c20cf4f686dd172248192ea1e25fecd832d
Christian Dräger <christian.draeger@idealo.de>
2017-06-10 21:21:50 +0200

    echo git deploy steps

--- changefile.sh
+++ changefile.sh
@@ -2,27 +2,32 @@
 YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
 DAY=$(date +"%d")
+
+echo "Make Sure we are in the Build Directory"
 cd $TRAVIS_BUILD_DIR
 
-#Remove Remotes Added by TravisCI
+echo "Remove Remotes Added by TravisCI"
 git remote rm origin
 
-#Add Remote with Secure Key
+echo "Add Remote with Secure Key"
 git remote add origin https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 
-#List Remotes ONLY DURING testing - do not do this on live repo / possible key leak
-#git remote -v
-
-# Set Git Variables
+echo "Set Git Variables"
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
 git config --global push.default simple
 
-# Make sure we have master branch checked out in Git
+echo "Make sure we have master branch checked out in Git"
 git checkout master
 
-# Add the modified file and commit it
+echo "adding $TRAVIS_BUILD_DIR/${JAR}"
 git add $TRAVIS_BUILD_DIR/"${JAR}"
+
+echo "adding $TRAVIS_BUILD_DIR/${DEB}"
 git add $TRAVIS_BUILD_DIR/"${DEB}"
+
+echo "adding $TRAVIS_BUILD_DIR/${EXE}"
 git add $TRAVIS_BUILD_DIR/"${EXE}"
+
+echo "commit all added files"
 git commit -am "$DAY-$MONTH-$YEAR.v$TRAVIS_BUILD_NUMBER [ci skip]"
--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
 DAY=$(date +"%d")
 
-# Make Sure we are in the Build Directory
+echo "Make Sure we are in the Build Directory"
 cd $TRAVIS_BUILD_DIR
 
-# Create Version Number
+echo "Create Version Number"
 export GIT_TAG=$DAY-$MONTH-$YEAR.v$TRAVIS_BUILD_NUMBER
 
-# Tag release
+echo "Tag release"
 git tag $GIT_TAG -a -m "$DAY-$MONTH-$YEAR.v$TRAVIS_BUILD_NUMBER"
 
-# Push commit and tags back to the repo
+echo "Push commit and tags back to the repo"
 sudo git push origin master && git push origin master --tags
